c88a03607cc72d013dcc8db2f84253524c38f387
houminwei <minwei.hou@bitmain.com>
2022-07-08 15:31:53 +0800

    feat：配置scss

--- src/pages/view.jsx
+++ src/pages/view.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import './view.css'
+import './view.scss'
 
 class View extends React.Component {
     render() {
         return (
             <div className='img-container'>
-               查看图片测试
+                查看图片测试
+                <header className='header'>
+                    4234
+                </header>
             </div>
         )
     }
