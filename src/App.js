9d742bbc881538aac51da4284ae19391008b9d63
houminwei <minwei.hou@bitmain.com>
2022-07-08 14:50:26 +0800

    feat：初始化项目

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import View from './pages/view';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className='img-container'>
+               <View></View>
+            </div>
+        )
+    }
+}
+
+export default App
---
+++ src/pages/view.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './view.css'
+
+class View extends React.Component {
+    render() {
+        return (
+            <div className='img-container'>
+               查看图片测试
+            </div>
+        )
+    }
+}
+
+export default View
